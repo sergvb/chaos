2709418a9fd7b68642058f1c1e8770e64cc0e6a1
serg <srg.trustno1@gmail.com>
2016-04-09 03:56:58 +0300

    The receiver is almost done.

---
+++ DCSK/Receiver.m
@@ -0,0 +1,63 @@
+classdef Receiver < handle
+    
+    properties
+        globalConfig;
+        mapper;
+        refPermuteMatrix;
+    end
+    
+    methods
+        
+        function this = Receiver(globalConfig, mapper)
+            this.globalConfig = globalConfig;
+            this.mapper = mapper;
+            this.refPermuteMatrix = this.CalculateRefPermuteMatrix();
+        end
+        
+        function [R, Y] = Receive(this, signal)
+            cfg = this.globalConfig;
+            
+            samplesBlocks = this.SerialToParallel(signal(:));
+            result = fft(samplesBlocks);
+            result = this.mapper.matrix.'*result;
+            R = this.refPermuteMatrix*result(1:cfg.P*cfg.Np, :);
+            Y = result((cfg.P*cfg.Np + 1):end, :);
+        end
+        
+    end
+    
+    methods(Access = private)
+        
+        function result = SerialToParallel(this, samples)
+            cfg = this.globalConfig;
+            
+            samplesNumber = numel(samples);
+            blocksNumber = ceil(samplesNumber/cfg.N);
+            alignedData = zeros(blocksNumber*cfg.N, 1);
+            alignedData(1:samplesNumber) = samples;
+            result = reshape(alignedData, cfg.N, []);
+        end
+        
+        function result = CalculateRefPermuteMatrix(this)
+            cfg = this.globalConfig;
+            
+            mat = zeros(cfg.P, cfg.Np);
+            mat(1, 1) = 1;
+            mat = repmat(mat, cfg.Np, cfg.P);
+            
+            for i = 0:cfg.P-1
+                cols = i*cfg.Np + (1:cfg.Np);
+                mat(:, cols) = circshift(mat(:, cols), i, 1);
+            end
+            
+            for i = 0:cfg.Np-1
+                rows = i*cfg.P + (1:cfg.P);
+                mat(rows, :) = circshift(mat(rows, :), i, 2);
+            end
+            
+            result = mat.';
+        end
+        
+    end
+    
+end
--- DCSK/test.m
+++ DCSK/test.m
@@ -1,12 +1,32 @@
- clc, clear all;
+clc, clear all;
 
-cfg = globalConfiguration();
+% cfg = globalConfiguration();
+% Spreading factor
+cfg.beta = 20;
+% Number of users
+cfg.P = 2;
+% Total number of subcarriers
+cfg.N = 16;
+% Number of private subcarriers for one user
+cfg.Np = 3;
+% Shared public subcarriers
+cfg.Ns = cfg.N - cfg.P*cfg.Np;
+
 mapper = CombTypeMapper(cfg);
-user = User(1, CPFChaoticGenerator(0.03));
-tx = Transmitter(cfg, user, mapper);
+user1 = User(1, CPFChaoticGenerator(0.03));
+user2 = User(2, CPFChaoticGenerator(0.05));
+tx1 = Transmitter(cfg, user1, mapper);
+tx2 = Transmitter(cfg, user2, mapper);
+rx = Receiver(cfg, mapper);
 
-data = (-1).^(1:33);
-signal = tx.Transmit(data);
-disp(data);
-%plot([real(signal).', imag(signal).']);
-plot(abs(fft(signal)).^2);
+data1 = [-1 -1 1 1 1 -1 1 1 -1 1 1 1 -1];
+data2 = [-1 1 -1 1 -1 1 1 1 -1 -1 -1 -1 1];
+signal = tx1.Transmit(data1) + tx2.Transmit(data2);
+% plot([real(signal).', imag(signal).']);
+
+[R, Y] = rx.Receive(signal);
+
+% D = kron(eye(cfg.P), ones(cfg.Np, 1)).'*R.'*Y;
+% D(D >= 1) = 1;
+% D(D <= -1) = -1;
+% disp(D);
